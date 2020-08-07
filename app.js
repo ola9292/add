afbf5793df9f4592b78e37c0ab3d3c290b295528
ola9292 <okaz692@gmail.com>
2020-08-07 23:25:51 +0100

    my add

---
+++ app.js
@@ -0,0 +1,37 @@
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+    }
+var body = document.querySelector('body');
+isBlue = false;
+setInterval(function(){
+    if(isBlue){
+        body.style.background = getRandomColor();
+    }else{
+        body.style.background = 'white';
+    }
+    isBlue = !isBlue;
+},1000);
+function calc(){
+    var a = parseInt(document.querySelector('#value1').value);
+    var b = parseInt(document.querySelector('#value2').value);
+    var op = document.querySelector('#operator').value;
+    var calculate;
+
+    if( op == 'add'){
+        calculate = a+b;
+    }else if(op == 'subtract'){
+        calculate = a - b;
+    }else if(op == 'divide'){
+        calculate = a/b;
+    }else if(op == 'multiply'){
+        calculate = a*b;
+    }
+ document.querySelector('#result').innerHTML = calculate;
+
+}
